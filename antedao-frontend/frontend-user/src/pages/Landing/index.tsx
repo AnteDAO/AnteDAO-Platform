f9908bc8190112dc0a62b6872c9f197a80b442b6
Sotatek-TuanPham2 <tuan.pham2@sotatek.com>
2022-08-12 15:05:09 +0700

    delete Lasquare

--- antedao-frontend/frontend-user/src/pages/Landing/index.tsx
+++ antedao-frontend/frontend-user/src/pages/Landing/index.tsx
@@ -15,15 +15,8 @@ import SliderComponent from "./Upcoming";
 import Completed from "./Completed";
 import SearchGroup from "../../components/Base/SearchGroup";
 import useFetch from "../../hooks/useFetch";
-import _ from "lodash";
 // import Slider from "react-slick";
 
-const urlApi = process.env.REACT_APP_API_LASQUARE;
-const url = process.env.REACT_APP_LASQUARE_DEEP_LINK;
-const urlView = process.env.REACT_APP_LASQUARE_VIEW_MORE_LINK;
-const urlDetail = process.env.REACT_APP_LASQUARE_POOL_DETAIL_LINK;
-
-type NFTData = [];
 type PoolData = {
   data: [];
   total: string;
@@ -35,9 +28,6 @@ const Dashboard = (props: any) => {
   const styles = useStyles();
   const [showModal, setShowModal] = useState(false);
 
-  const { data: lasquareNFT, loading: loadingLasquareNFT } = useFetch<NFTData>(
-    `${urlApi}v1/nfts?limit=6&sort=RECENT`
-  );
   const [upcomingPoolsV3Display, setUpcomingPoolsV3Display] = useState<
     Array<any>
   >([]);
@@ -146,87 +136,6 @@ const Dashboard = (props: any) => {
               )}
               <FormSubmitToSheet />
             </div>
-          </div>
-        </div>
-        <div className={styles.lasquare}>
-          <div className="lasquare_info">
-            <p className="title">Lasquare</p>
-            <p className="description">
-              The NFT Marketplace aims to provide a platform for users to trade
-              their digital creations as NFTs (non-fungible token), allowing a
-              safe and secure environment for creators to generate funding for
-              their projects, as well as collectors to purchase and collect
-              digital assets.
-            </p>
-          </div>
-
-          <div>
-            {/* <Grid container spacing={3}> */}
-            {lasquareNFT && lasquareNFT.length > 0 && (
-              <Grid container className="center_align">
-                <Grid
-                  container
-                  item
-                  xs={12}
-                  md={6}
-                  spacing={3}
-                  className="left"
-                >
-                  {_.take(lasquareNFT, 3).map((pool: any, index) => {
-                    return (
-                      <Grid item xs={6} key={index}>
-                        <div className="item_lasquare">
-                          <a
-                            href={`${urlDetail}${pool.id}`}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <img src={`${url}${pool.largeImage}`} alt="" />
-                          </a>
-
-                          <p className="name">{pool.name}</p>
-                        </div>
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-                <Grid
-                  container
-                  item
-                  xs={12}
-                  md={6}
-                  spacing={3}
-                  className="right"
-                >
-                  {_.takeRight(lasquareNFT, 3).map((pool: any, index) => {
-                    return (
-                      <Grid item xs={6} key={index}>
-                        <div className="item_lasquare">
-                          <a
-                            href={`${urlDetail}${pool.id}`}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <img src={`${url}${pool.largeImage}`} alt="" />
-                          </a>
-                          <p className="name">{pool.name}</p>
-                        </div>
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-              </Grid>
-            )}
-          </div>
-          <div className="view_more">
-            <a
-              href={urlView}
-              className="btn btn-view"
-              target="_blank"
-              rel="noreferrer"
-            >
-              View more
-            </a>
           </div>
         </div>
         {showModal && <ModalContent setShowModal={setShowModal} />}
